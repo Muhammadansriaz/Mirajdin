ba98e6e82d2fe90909e912851c26f1477bb6e0a9
Muhammad Ans <ansriaz444@gmail.com>
2021-11-23 23:22:55 +0500

    rmv

--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -4,7 +4,7 @@ import style from "./Home.module.css";
 function Home() {
   return (
     <div className={style.homeText}>
-      <h1 className={style.headingText}>Welcome to Mirajdin Engineering!</h1>
+      <h1 className={style.headingText}>Welcome to Mirajdin Engineering</h1>
       <p className={style.paraText}>The Agricultural Machinery Experts.</p>
     </div>
   );
